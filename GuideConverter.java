e142fee92e21fc1a609f234b4ba63085813772a4
yasmin-aumeeruddy <52958137+yasmin-aumeeruddy@users.noreply.github.com>
2020-06-11 11:11:48 +0100

    Update GuideConverter.java

--- GuideConverter.java
+++ GuideConverter.java
@@ -166,7 +166,8 @@ public class GuideConverter{
     //inserts code snippet 
     public static void codeSnippet(String path, String guideName){
         try{
-            String httpsURL = "https://raw.githubusercontent.com/OpenLiberty/"+guideName+"/master/finish/"+path;
+            //CHANGE URL TO OPEN LIBERTY AFTER DEMO
+            String httpsURL = "https://raw.githubusercontent.com/yasmin-aumeeruddy/"+guideName+"/master/finish/"+path;
             String FILENAME = "c:\\temp\\filename.adoc";
             BufferedWriter bw = new BufferedWriter(new FileWriter(FILENAME));
             URL myurl = new URL(httpsURL);
